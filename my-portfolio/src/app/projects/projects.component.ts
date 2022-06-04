23142555752fc620213955a3adf57da901bd12c8
johnvillar41 <johndanielvillar123@gmail.com>
2022-06-04 08:04:18 +0800

    updated projects

--- my-portfolio/src/app/projects/projects.component.ts
+++ my-portfolio/src/app/projects/projects.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProjectModel } from 'src/models/projectModel';
 
 @Component({
   selector: 'app-projects',
@@ -7,9 +8,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectsComponent implements OnInit {
 
+  projects: ProjectModel[] = [];
   constructor() { }
 
   ngOnInit(): void {
+    let projectFlameAndWax: ProjectModel = {
+      projectTitle: "Flame And Wax",
+      projectDescription: "A website e-commerce application created using .NetCore MVC Framework C# Application.",
+      githubLink: "https://github.com/johnvillar41/FlameAndWax-Web-Application",
+      isDark: true
+    }
+    let projectBGCMobile: ProjectModel = {
+      projectTitle: "BGC Mobile Development",
+      projectDescription: "A android e-commerce application created using Native Android Framework and Java Language.",
+      githubLink: "https://github.com/johnvillar41/BGC-Mobile-Development",
+      isDark: false
+    }
+    let projectBGCWeb: ProjectModel = {
+      projectTitle: "BGC Web Development",
+      projectDescription: "A website e-commerce application created using ASP.NET Webforms Framework and C# Language.",
+      githubLink: "https://github.com/johnvillar41/BGC-Web-Development",
+      isDark: false
+    }
+    let projectSentimentAnalysisWeb: ProjectModel = {
+      projectTitle: "Sentiment Analysis Tool Web",
+      projectDescription: "A website created for Computing sentiments using .NetCore MVC Framework C# Application.",
+      githubLink: "https://github.com/johnvillar41/SentimentAnalysisTool.Web",
+      isDark: true
+    }
+    let projectSentimentAnalysisApi: ProjectModel = {
+      projectTitle: "Sentiment Analysis Tool Api",
+      projectDescription: "An API for Computing sentiments using .NetCore Web API Framework C# Application.",
+      githubLink: "https://github.com/johnvillar41/SentimentAnalysisTool.Api",
+      isDark: true
+    }
+    this.projects.push(projectFlameAndWax);
+    this.projects.push(projectBGCMobile);
+    this.projects.push(projectBGCWeb);
+    this.projects.push(projectSentimentAnalysisWeb);
+    this.projects.push(projectSentimentAnalysisApi);
   }
 
 }
---
+++ my-portfolio/src/models/projectModel.ts
@@ -0,0 +1,6 @@
+export interface ProjectModel {
+  projectTitle: string;
+  projectDescription: string;
+  githubLink: string;
+  isDark: boolean;
+}
