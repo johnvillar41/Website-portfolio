feb16b436a575b207645278969b16137865efba1
johnvillar41 <johndanielvillar123@gmail.com>
2022-06-04 08:49:49 +0800

    updated certifications

--- my-portfolio/src/app/certifications/certifications.component.ts
+++ my-portfolio/src/app/certifications/certifications.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CertificationModel } from 'src/models/certificationModel';
 
 @Component({
   selector: 'app-certifications',
@@ -7,9 +8,70 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CertificationsComponent implements OnInit {
 
+  certifications: CertificationModel[] = [];
   constructor() { }
 
   ngOnInit(): void {
+    this.onInitializeCertifications();
   }
+
+  onInitializeCertifications(): void {
+    let machineVisionCert: CertificationModel = {
+      certificationTitle: "Machine Vision",
+      certificationImageLink: "https://johnvillar41.github.io/Website-portfolio/assets/images/Cert_Machine_Vision.jpg"
+    };
+    let gitCert: CertificationModel = {
+      certificationTitle: "Better Code Management and Collaboration: Introduction to Git",
+      certificationImageLink: "https://johnvillar41.github.io/Website-portfolio/assets/images/Better%20code.PNG"
+    };
+    let uiUxCert: CertificationModel = {
+      certificationTitle: "UI/UX Creating a Design System",
+      certificationImageLink: "https://johnvillar41.github.io/Website-portfolio/assets/images/Ui_UX_CERT.png"
+    };
+    let cloudCert: CertificationModel = {
+      certificationTitle: "AWS Educate Introduction to Cloud 101",
+      certificationImageLink: "https://johnvillar41.github.io/Website-portfolio/assets/images/Aws_CloudComputing.PNG"
+    };
+    let learningDesignThinkingCert: CertificationModel = {
+      certificationTitle: "Learning Design Thinking: Lead Change in Your Organization",
+      certificationImageLink: "https://johnvillar41.github.io/Website-portfolio/assets/images/CertificateOfCompletion_Learning%20Design%20Thinking%20Lead%20Change%20in%20Your%20Organization-1.png"
+    };
+    let creativeThinkingCert: CertificationModel = {
+      certificationTitle: "Develop Your Creative Thinking and Innovation Skills",
+      certificationImageLink: "https://johnvillar41.github.io/Website-portfolio/assets/images/CertificateOfCompletion_Develop%20Your%20Creative%20Thinking%20and%20Innovation%20Skills%20(1)-1.png"
+    };
+    let criticalThinkingCert: CertificationModel = {
+      certificationTitle: "Develop Critical Thinking DecisionMaking and ProblemSolving Skills",
+      certificationImageLink: "https://johnvillar41.github.io/Website-portfolio/assets/images/CertificateOfCompletion_Develop%20CriticalThinking%20DecisionMaking%20and%20ProblemSolving%20Skills-1.png"
+    };
+    let projectManagementFoundationsCert: CertificationModel = {
+      certificationTitle: "Project Management Foundations",
+      certificationImageLink: "https://johnvillar41.github.io/Website-portfolio/assets/images/CertificateOfCompletion_Project%20Management%20Foundations-1.png"
+    };
+    let projectManagerCert: CertificationModel = {
+      certificationTitle: "Become an Agile Project Manager",
+      certificationImageLink: "https://johnvillar41.github.io/Website-portfolio/assets/images/CertificateOfCompletion_Become%20an%20Agile%20Project%20Manager-1.png"
+    };
+    let cloudDeveloperCert: CertificationModel = {
+      certificationTitle: "Become a Cloud Developer",
+      certificationImageLink: "https://johnvillar41.github.io/Website-portfolio/assets/images/CertificateOfCompletion_Become%20a%20Cloud%20Developer-1.png"
+    };
+    this.certifications.push(machineVisionCert);
+    this.certifications.push(gitCert);
+    this.certifications.push(uiUxCert);
+    this.certifications.push(cloudCert);
+    this.certifications.push(learningDesignThinkingCert);
+    this.certifications.push(creativeThinkingCert);
+    this.certifications.push(criticalThinkingCert);
+    this.certifications.push(projectManagementFoundationsCert);
+    this.certifications.push(projectManagerCert);
+    this.certifications.push(cloudDeveloperCert);
+  }
+
+  onImageLinkClicked(certificationLink: string): void {
+    window.open(certificationLink, '_blank');
+  }
+
+
 
 }
---
+++ my-portfolio/src/models/certificationModel.ts
@@ -0,0 +1,4 @@
+export interface CertificationModel {
+  certificationTitle: string;
+  certificationImageLink: string;
+}
