869b22951ac6c7eb5b80094722407f864acaa921
johnvillar41 <johndanielvillar123@gmail.com>
2022-06-11 13:04:45 +0800

    updated imagelinks

--- my-portfolio/src/app/practicum-overview/practicum-overview.component.ts
+++ my-portfolio/src/app/practicum-overview/practicum-overview.component.ts
@@ -15,28 +15,28 @@ export class PracticumOverviewComponent implements OnInit {
 
   onInitializeRequiredLearningModules(): void {
     let problemSolving: LearningModuleModel = {
-      imageLink: '../../assets/images/linkedinlogo.png',
+      imageLink: 'https://johnvillar41.github.io/Website-portfolio/assets/images/linkedinlogo.png',
       moduleTitle: "Problem Solving Techniques",
       moduleDescription: "LinkedIn Learning By: Chris Croft Apr 2017",
       dateFinished: '6/2/2022',
       isDark: true
     }
     let designThinking: LearningModuleModel = {
-      imageLink: '../../assets/images/linkedinlogo.png',
+      imageLink: 'https://johnvillar41.github.io/Website-portfolio/assets/images/linkedinlogo.png',
       moduleTitle: "Learning Design Thinking: Lead Change in Your Organization",
       moduleDescription: "Learning Design Thinking: Lead Change in Your Organization",
       dateFinished: '5/27/2022',
       isDark: false
     }
     let creativeThinking: LearningModuleModel = {
-      imageLink: '../../assets/images/linkedinlogo.png',
+      imageLink: 'https://johnvillar41.github.io/Website-portfolio/assets/images/linkedinlogo.png',
       moduleTitle: "Develop Your Creative Thinking and Innovation Skills",
       moduleDescription: "LinkedIn Learning Apr 2020",
       dateFinished: '5/7/2022',
       isDark: true
     }
     let criticalThinking: LearningModuleModel = {
-      imageLink: '../../assets/images/linkedinlogo.png',
+      imageLink: 'https://johnvillar41.github.io/Website-portfolio/assets/images/linkedinlogo.png',
       moduleTitle: "Develop Critical-Thinking, Decision-Making, and Problem-Solving Skills",
       moduleDescription: "LinkedIn Learning May 2020",
       dateFinished: '6/2/2022',
@@ -50,14 +50,14 @@ export class PracticumOverviewComponent implements OnInit {
 
   onInitializeProjectManagementLearningModules(): void {
     let projectManagementFoundations: LearningModuleModel = {
-      imageLink: '../../assets/images/linkedinlogo.png',
+      imageLink: 'https://johnvillar41.github.io/Website-portfolio/assets/images/linkedinlogo.png',
       moduleTitle: "Project Management Foundations",
       moduleDescription: "Bonnie Biafore Jun 2019",
       dateFinished: '5/21/2022',
       isDark: true
     }
     let agileProjectManager: LearningModuleModel = {
-      imageLink: '../../assets/images/linkedinlogo.png',
+      imageLink: 'https://johnvillar41.github.io/Website-portfolio/assets/images/linkedinlogo.png',
       moduleTitle: "Become an Agile Project Manager",
       moduleDescription: "LinkedIn Learning May 2019",
       dateFinished: '5/21/2022',
@@ -69,14 +69,14 @@ export class PracticumOverviewComponent implements OnInit {
 
   onInitializeSpecializationModules(): void {
     let uxUi: LearningModuleModel = {
-      imageLink: '../../assets/images/linkedinlogo.png',
+      imageLink: 'https://johnvillar41.github.io/Website-portfolio/assets/images/linkedinlogo.png',
       moduleTitle: "Improve Your UX Design Skills",
       moduleDescription: "LinkedIn Learning Jun 2018",
       dateFinished: '5/4/2022',
       isDark: true
     }
     let projectManagementFoundations: LearningModuleModel = {
-      imageLink: '../../assets/images/linkedinlogo.png',
+      imageLink: 'https://johnvillar41.github.io/Website-portfolio/assets/images/linkedinlogo.png',
       moduleTitle: "Project Management Foundations",
       moduleDescription: "LinkedIn Learning Apr 2019",
       dateFinished: '5/15/2022',
